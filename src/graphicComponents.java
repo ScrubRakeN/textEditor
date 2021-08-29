83587f99bd66332c3b93c8066b40787acc0de731
= <=>
2021-08-29 18:26:12 -0300

    Final version

---
+++ src/graphicComponents.java
@@ -0,0 +1,346 @@
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class graphicComponents implements LayoutManager{
+
+	private JLabel label;
+	private Container container;
+	private ImageIcon image;
+	private JTextArea textArea;
+	private JTextField textField;
+	private JFormattedTextField fotmattedTextField;
+	private FileReader fileReader;
+	private FileDialog fileDialog;
+	private JButton button;
+	private JPanel pane;
+	private JFrame frame;
+	private JTable table;
+	private JScrollPane scrollPane, textScroll;
+	private JTabbedPane tabbedPane;
+	private JCheckBox check;
+	private JSpinner spinner;
+	private JRadioButton radio;
+	private ButtonGroup group;
+	private JComboBox comboBox;
+	private JList list;
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem menuItem;
+	
+	
+	//Instanciating Components
+	
+	public graphicComponents newFrame(int width, int height, String title, boolean resizable, String url) {
+		
+		frame = new JFrame();
+		frame.setTitle(title);
+		frame.setIconImage(newImage(url).getImage());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(width, height);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(resizable);
+		frame.setVisible(true);
+		
+		return this;
+		
+	}
+	
+	public JPanel newPanel() {
+		
+		pane = new JPanel();
+		pane.setLayout(null);
+		pane.setVisible(true);
+		
+		return pane;
+		
+	}
+	
+	public JLabel newLabel(int x, int y, int width, int height, String caption, String url) {
+		
+		label = new JLabel(caption);
+		label.setIcon(newImage(url));
+		label.setFont(new Font("Arial", Font.BOLD, 17));
+		label.setBounds(x, y, width, height);
+		label.setVisible(true);
+		
+		return label;
+		
+	}
+	
+	public JTextField newTextField(int x, int y, int width, int height, String caption) {
+		
+		textField = new JTextField(caption);
+		textField.setBounds(x, y, width, height);
+		textField.setVisible(true);
+		
+		return textField;
+		
+	}
+	
+	public JScrollPane newScrollPane(int x, int y, int width, int height, String[] items) {
+		
+		list = new JList(items);
+		scrollPane = new JScrollPane(list);
+		list.setSelectedIndex(0);
+		scrollPane.setBounds(x, y, width, height);
+		scrollPane.setFocusable(false);
+		
+		return scrollPane;
+		
+	}
+	
+	public JComboBox newComboBox(int x, int y, int width, int height, String[] items) {
+		
+		comboBox = new JComboBox(items);
+		comboBox.setBounds(x, y, width, height);
+		comboBox.setFocusable(false);
+		
+		return comboBox;
+	}
+	
+	public JSpinner newSpinner(int x, int y, int width, int height) {
+		
+		spinner = new JSpinner();
+		spinner.setValue(20);
+		spinner.setBounds(x, y, width, height);
+		
+		return spinner;
+	}
+	
+	public JButton newButton(int x, int y, int width, int height, String caption) {
+		
+		button = new JButton(caption);
+		button.setBounds(x, y, width, height);
+		button.setFocusable(false);
+		button.setVisible(true);
+
+		return button;
+		
+	}
+	
+	public JScrollPane newTextArea(int x, int y, int width, int height) {
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		textArea.setWrapStyleWord(true);
+		
+		textScroll = new JScrollPane(textArea);
+		textScroll.setBounds(x, y, width, height);
+		textScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		
+		return textScroll;
+		
+	}
+	
+	public graphicComponents newMenuBar() {
+		
+		menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		return this;
+	}
+	
+	public JMenu newMenu(String menuCaption) {
+		
+		menu = new JMenu(menuCaption);
+		return menu;
+	}
+	
+	public JMenuItem newMenuItem(String itemCaption, int keyEvent, int eventMask) {
+		
+		menuItem = new JMenuItem(itemCaption);
+		menuItem.setAccelerator(KeyStroke.getKeyStroke(keyEvent, eventMask));
+		
+		return menuItem;
+	}
+	
+	public ImageIcon newImage(String url) {
+		
+		image = new ImageIcon(url);
+		
+		return image;
+		
+	}
+	
+	public FileDialog newFileDialog(String caption, int mode) {
+		
+		fileDialog = new FileDialog(frame , caption, mode);
+		
+		return fileDialog;
+	}
+	
+	//LayoutManager Methods
+	@Override
+	public void addLayoutComponent(String name, Component comp) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void removeLayoutComponent(Component comp) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Dimension preferredLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Dimension minimumLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void layoutContainer(Container parent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//Graphic Components Getters and Setters
+	
+	public JLabel getLabel() {
+		return label;
+	}
+	public void setLabel(JLabel label) {
+		this.label = label;
+	}
+	public Container getContainer() {
+		return container;
+	}
+	public void setContainer(Container container) {
+		this.container = container;
+	}
+	public ImageIcon getImage() {
+		return image;
+	}
+	public void setImage(ImageIcon image) {
+		this.image = image;
+	}
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+	public void setTextArea(TextArea JtextArea) {
+		this.textArea = textArea;
+	}
+	public JTextField getTextField() {
+		return textField;
+	}
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
+	}
+	public JFormattedTextField getFotmattedTextField() {
+		return fotmattedTextField;
+	}
+	public void setFotmattedTextField(JFormattedTextField fotmattedTextField) {
+		this.fotmattedTextField = fotmattedTextField;
+	}
+	public FileReader getFileReader() {
+		return fileReader;
+	}
+	public void setFileReader(FileReader fileReader) {
+		this.fileReader = fileReader;
+	}
+	public JButton getButton() {
+		return button;
+	}
+	public void setButton(JButton button) {
+		this.button = button;
+	}
+	public JPanel getPane() {
+		return pane;
+	}
+	public void setPane(JPanel pane) {
+		this.pane = pane;
+	}
+	public JFrame getFrame() {
+		return frame;
+	}
+	public void setFrame(JFrame frame) {
+		this.frame = frame;
+	}
+	public JTable getTable() {
+		return table;
+	}
+	public void setTable(JTable table) {
+		this.table = table;
+	}
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
+	public void setScrollPane(JScrollPane scroll) {
+		this.scrollPane = scroll;
+	}
+	public JTabbedPane getTabbedPane() {
+		return tabbedPane;
+	}
+	public void setTabbedPane(JTabbedPane tabbedPane) {
+		this.tabbedPane = tabbedPane;
+	}
+	public JCheckBox getCheckBox() {
+		return check;
+	}
+	public void setCheckBox(JCheckBox check) {
+		this.check = check;
+	}
+	public ButtonGroup getGroup() {
+		return group;
+	}
+	public void setGroup(ButtonGroup group) {
+		this.group = group;
+	}
+	public JRadioButton getRadioButton() {
+		return radio;
+	}
+	public void setRadioButton(JRadioButton radio) {
+		this.radio = radio;
+	}
+	public JComboBox getComboBox() {
+		return comboBox;
+	}
+	public void setComboBox(JComboBox combo) {
+		this.comboBox = combo;
+	}
+	public JList getList() {
+		return list;
+	}
+	public void setList(JList list) {
+		this.list = list;
+	}
+	public JMenuBar getMenuBar() {
+		return menuBar;
+	}
+	public void setMenuBar(JMenuBar menuBar) {
+		this.menuBar = menuBar;
+	}
+	public JMenu getMenu() {
+		return menu;
+	}
+	public void setMenu(JMenu menu) {
+		this.menu = menu;
+	}
+	public JMenuItem getMenuItem() {
+		return menuItem;
+	}
+	public void setMenuItem(JMenuItem menuItem) {
+		this.menuItem = menuItem;
+	}
+
+	public FileDialog getFileDialog() {
+		return fileDialog;
+	}
+
+	public void setFileDialog(FileDialog fileDialog) {
+		this.fileDialog = fileDialog;
+	}
+
+	public JSpinner getSpinner() {
+		return spinner;
+	}
+
+	public void setSpinner(JSpinner spinner) {
+		this.spinner = spinner;
+	}
+
+}
---
+++ src/textEditor.java
@@ -0,0 +1,319 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.font.TextAttribute;
+
+public class textEditor {
+		
+	private static graphicComponents gComponents = new graphicComponents(); 
+	
+	public static Component itialilizingComponents() {
+		
+		gComponents.newPanel();
+		gComponents.newMenuBar();
+		
+		gComponents.getMenuBar().add(gComponents.newMenu("File"));
+		
+		gComponents.newFileDialog("Save archive", FileDialog.SAVE);
+		gComponents.getMenu().add(gComponents.newMenuItem("Save", KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		gComponents.getMenuItem().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try {
+					
+					gComponents.getFileDialog().setVisible(true);
+					if (gComponents.getFileDialog().getFile() == null) {
+						return;
+					}
+					
+					String archiveName = gComponents.getFileDialog().getDirectory() + gComponents.getFileDialog().getFile();
+					FileWriter out = new FileWriter(archiveName);
+					out.write(gComponents.getTextArea().getText());
+					out.close();
+					JOptionPane.showMessageDialog(null, "Successfully Saved!", "Status", JOptionPane.WARNING_MESSAGE);
+					
+				} catch (IOException error) {
+					
+					JOptionPane.showMessageDialog(null, "There was an error saving the archive\n" + error.toString(), "Status", JOptionPane.WARNING_MESSAGE);
+					
+				}
+				
+				
+			}
+		});
+		
+		
+		gComponents.newFileDialog("Load archive", FileDialog.LOAD);
+		gComponents.getMenu().add(gComponents.newMenuItem("Open", KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+		gComponents.getMenuItem().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				try {
+					gComponents.getFileDialog().setVisible(true);
+					if (gComponents.getFileDialog().getFile() == null) {
+						return;
+					}
+					String archiveName = gComponents.getFileDialog().getDirectory() + gComponents.getFileDialog().getFile();
+					FileReader in = new FileReader(archiveName);
+					String s = "";
+					int i = in.read();
+					while (i != -1) {
+						s += (char) i;
+						i = in.read();
+					}
+					gComponents.getTextArea().setText(s);
+					in.close();
+				} catch (IOException erro) {
+					JOptionPane.showMessageDialog(null, "There was an error opening the archive\n" + erro.toString(), "Status", JOptionPane.WARNING_MESSAGE);
+				}
+
+				
+			}
+		});
+		
+		
+		gComponents.getMenu().add(gComponents.newMenuItem("Exit", KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		gComponents.getMenuItem().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				System.exit(0);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newTextArea(10, 10, 700, 500));
+		
+		
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		String[] fonts = ge.getAvailableFontFamilyNames();
+		
+		
+		gComponents.getPane().add(gComponents.newSpinner(745, 30, 40, 20));
+		gComponents.getSpinner().addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()));
+				
+			}
+			
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newComboBox(800, 30, 100, 20, fonts));
+		gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()));
+		gComponents.getComboBox().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()));
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(745, 60, 45, 30, "B"));
+		gComponents.getButton().setFont(new Font("Arial", Font.BOLD, 14));
+		gComponents.getButton().addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+			
+				Font font = new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.BOLD, (int) gComponents.getSpinner().getValue());
+				
+				if(gComponents.getTextArea().getFont().equals(font)) {
+					
+					gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()));
+					
+				}else {
+					
+					gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.BOLD, (int) gComponents.getSpinner().getValue()));
+					
+				}
+				
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(800, 60, 45, 30, "I"));
+		gComponents.getButton().setFont(new Font("Times New Roman", Font.ITALIC, 20));
+		gComponents.getButton().addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+					
+				Font font = new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.ITALIC, (int) gComponents.getSpinner().getValue());
+				
+				if(gComponents.getTextArea().getFont().equals(font)) {
+					
+					gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()));
+				}else {
+					
+					gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.ITALIC, (int) gComponents.getSpinner().getValue()));
+					
+				}
+					
+			}
+		});
+	
+		Map<TextAttribute, Integer> fontAttributes = new HashMap<TextAttribute, Integer>();
+		fontAttributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		
+		gComponents.getPane().add(gComponents.newButton(855, 60, 45, 30, "U"));
+		gComponents.getButton().setFont(new Font("Arial", Font.BOLD, 14).deriveFont(fontAttributes));
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Font font = new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()).deriveFont(fontAttributes);
+				
+				if(gComponents.getTextArea().getFont().equals(font)) {
+					
+					fontAttributes.put(TextAttribute.UNDERLINE, -1);
+					
+				}
+					
+					gComponents.getTextArea().setFont(new Font(fonts[gComponents.getComboBox().getSelectedIndex()], Font.PLAIN, (int) gComponents.getSpinner().getValue()).deriveFont(fontAttributes));
+					
+					fontAttributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+					
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(745, 100, 45, 30, null));
+		gComponents.getButton().setBackground(Color.BLACK);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.BLACK);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(800, 100, 45, 30, null));
+		gComponents.getButton().setBackground(Color.WHITE);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.WHITE);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(745, 140, 45, 30, null));
+		gComponents.getButton().setBackground(Color.RED);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.RED);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(800, 140, 45, 30, null));
+		gComponents.getButton().setBackground(Color.GREEN);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.GREEN);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(745, 180, 45, 30, null));
+		gComponents.getButton().setBackground(Color.BLUE);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.BLUE);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(800, 180, 45, 30, null));
+		gComponents.getButton().setBackground(Color.YELLOW);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.YELLOW);
+				
+			}
+		});
+		
+		gComponents.getPane().add(gComponents.newButton(855, 100, 45, 30, null));
+		gComponents.getButton().setBackground(Color.GRAY);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.GRAY);
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(855, 140, 45, 30, null));
+		gComponents.getButton().setBackground(Color.MAGENTA);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.MAGENTA);
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(855, 180, 45, 30, null));
+		gComponents.getButton().setBackground(Color.ORANGE);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.ORANGE);
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(745, 220, 45, 30, null));
+		gComponents.getButton().setBackground(Color.PINK);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.PINK);
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(800, 220, 45, 30, null));
+		gComponents.getButton().setBackground(Color.CYAN);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.CYAN);
+				
+			}
+		});
+		
+		
+		gComponents.getPane().add(gComponents.newButton(855, 220, 45, 30, null));
+		gComponents.getButton().setBackground(Color.LIGHT_GRAY);
+		gComponents.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				gComponents.getTextArea().setForeground(Color.LIGHT_GRAY);
+				
+			}
+		});
+		
+		return gComponents.getPane();
+	}
+	
+	public static void main(String []args) {
+		
+		gComponents.newFrame(1000, 580, "BRAND NEW REVOLUTIONARY TEXT EDITOR", false, "C:\\Users\\CLIENTE\\Downloads\\wordPad.png");
+		gComponents.getFrame().getContentPane().add(itialilizingComponents());
+		
+	}
+}
+
+
